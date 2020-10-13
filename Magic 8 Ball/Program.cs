13e69e3aec7aa408be5533508c0a697123b0e040
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-10-13 08:15:26 -0400

    Add project files.

---
+++ Magic 8 Ball/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Magic_8_Ball
+{
+    class Program
+    {
+        static Random randomObject = new Random();
+        static void Main(string[] args)
+        {
+            QuestionLoop();
+        }
+        static string GetQuestionFromUser()
+        {
+            Console.Write("What question do you have? ");
+            string questionString = Console.ReadLine();
+            return questionString;
+        }
+        static void QuestionLoop()
+        {
+            while (true)
+            {
+                string questionString = GetQuestionFromUser();
+                int randomNumber = randomObject.Next(4);
+                switch (randomNumber)
+                {
+                    case 0:
+                        { 
+                        Console.WriteLine("Yes.");
+                        break;
+                        }
+                    case 1:
+                        {
+                            Console.WriteLine("No bro");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Bad idea");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("that's a mystery, no idea.");
+                            break;
+                        }
+                }
+            }
+   
+        }
+    }
+}
